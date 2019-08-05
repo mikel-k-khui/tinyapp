a2dfdc712b3ab79cf0875e8cfe599d8c28ffa4f7
mike chui <mikel.k.khui@gmail.com>
2019-08-05 23:36:04 +0000

    Updated redirect logic

--- express_server.js
+++ express_server.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 //setup Express app to use ejs as templating engine
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+    return Math.floor((1 + Math.random()) * 0x1000 * 0x1000).toString(16).substring(1,7);
+};
+
+function addURL(address) {
+  let newKey = generateRandomString();
+  urlDatabase[newKey] = address;
+  console.log(urlDatabase);
+  return newKey;
+};
+
+/*
+*
+*/
 app.get("/", (request, response) => {
   response.end("Hello!");
 });
@@ -18,11 +35,20 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/hello", (requ, resp) => {
   let template = { greeting : 'Hello World'};
   resp.render("hello_world", template);
 });
-//send respond about getting url for selected route
+//send respond about getting url for selected routes
 
 app.get("/set", (req, res) => {
   const a = 1;
@@ -55,4 +81,13 @@ app.get("/urls.json", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  let tempURL = 'urls/' + addURL(req.body["longURL"]);
+  console.log(tempURL);  // Log the POST request body to the console
+
+  res.redirect(302, tempURL);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
