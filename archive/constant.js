695cd2bcd83eb4b1224215d687c633c9c8fa70ac
Mike Chui <mikel.k.khui@gmail.com>
2019-08-08 18:40:36 -0600

    Removed pre-set data and copied it to archive

---
+++ archive/constant.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+
+const users = {
+  "1234": {
+    id: "1234",
+    email: "me@me.com",
+    password: bcrypt.hashSync("bob", 10)
+  },
+  "abcd": {
+    id: "abcd",
+    email: "i@me.com",
+    password: bcrypt.hashSync("bob", 10)
+  }
+};
+
+const urlDatabase = {
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "1234" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "1234"},
+  "g82ms9": { longURL: "http://www.triathlon.org", userID: "abcd"}
+};
+
+module.exports = { users, urlDatabase };
